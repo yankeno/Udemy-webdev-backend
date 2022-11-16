0241bf6601bbd33ab78b90e2cb7d5d4ccc47a725
yankeno <kn5sttyryh@gmail.com>
2022-11-16 21:51:59 +0900

    [add]演習用ファイルの追加
    ・演習用ファイル
    ・メモ

---
+++ src/index.php
@@ -0,0 +1,5 @@
+<form action="get.php" method="GET">
+    name: <input type="text" name="username"><br>
+    password: <input type="password" name="pwd"><br>
+    <button type="submit">send</button>
+</form>
